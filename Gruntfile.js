8b266e1e05afd744cc4cc9d6072d452f47614b61
Leo D <37496762+LeoDaza7@users.noreply.github.com>
2018-05-14 12:05:54 -0400

    CI-001: grunt project configured, html files added.

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function (grunt) {
+
+	var config = grunt.file.readJSON('config.json' || grunt.option('config'));
+
+	grunt.registerTask('formatPage1',function(){
+	
+		grunt.file.copy('page1.html', config.buildFolder + '/' + config.pageOneName + '.html',{
+
+			process: function funcion(fileContent) {
+
+				return grunt.template.process(fileContent,{
+
+					data: {
+
+						pageTitle : config.appName,
+
+					},
+
+				});
+
+			}
+
+		});
+
+	});
+
+	grunt.registerTask('formatPage2',function(){
+	
+		grunt.file.copy('page2.html', config.buildFolder + '/' + config.pageTwoName + '.html',{
+
+			process: function funcion(fileContent) {
+
+				return grunt.template.process(fileContent,{
+
+					data: {
+
+						pageTitle : config.appName,
+
+					},
+
+				});
+
+			}
+
+		});
+
+	});
+
+		grunt.registerTask('formatIndex',function(){
+	
+		grunt.file.copy('index.html', config.buildFolder + '/index.html',{
+
+			process: function funcion(fileContent) {
+
+				return grunt.template.process(fileContent,{
+
+					data: {
+
+						pageTitle : config.appName,
+
+					},
+
+				});
+
+			}
+
+		});
+
+	});
+
+	grunt.registerTask('build',['formatPage1','formatPage2','formatIndex']);
+
+};
